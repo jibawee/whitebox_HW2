f11564da07bb42e22036e61d9b3336c6456b8e9e
jibawee <teakidder@gmail.com>
2025-07-13 14:10:38 -0700

    settin up

--- tests.py
+++ tests.py
@@ -1,5 +1,13 @@
 import unittest
 from contrived_func import contrived_func
 
+class TestContrivedFunc(unittest.TestCase):
+
+    def test_case_1(self):
+        contrived_func(...)  
+
+    def test_case_2(self):
+        contrived_func(...)
+
 if __name__ == '__main__':
     unittest.main()
