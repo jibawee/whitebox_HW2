c71eaf925b7352dc9249c91d309641c43a4daec8
Tea Kidder <teakidder@gmail.com>
2025-07-13 19:59:36 -0700

    testin

--- tests.py
+++ tests.py
@@ -4,10 +4,10 @@ from contrived_func import contrived_func
 class TestContrivedFunc(unittest.TestCase):
 
     def test_case_1(self):
-        contrived_func(...)  
+        contrived_func(0)  
 
     def test_case_2(self):
-        contrived_func(...)
+        contrived_func(1)
 
 if __name__ == '__main__':
     unittest.main()
