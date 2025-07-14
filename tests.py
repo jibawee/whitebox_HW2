92666840a6dce240c4042d6cba1846d14378a389
Tea Kidder <teakidder@gmail.com>
2025-07-13 20:29:34 -0700

    maybe large numba

--- tests.py
+++ tests.py
@@ -12,11 +12,17 @@ class TestContrivedFunc(unittest.TestCase):
     def test_case_minus_1(self):
         contrived_func(-1)
 
+    def test_case_double(self):
+        contrived_func(1.5)
+
     def test_case_100(self):
         contrived_func(100)
 
-    def test_case_double(self):
-        contrived_func(1.5)
+    def test_case_100(self):
+        contrived_func(100)
+    
+    def test_case_100(self):
+        contrived_func(784907584903)
 
 if __name__ == '__main__':
     unittest.main()
