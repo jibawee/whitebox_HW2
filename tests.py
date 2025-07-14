c5a1c2b71d8711c0f0d8520fe64a46ef7bcbe937
Tea Kidder <teakidder@gmail.com>
2025-07-13 20:06:33 -0700

    more tests

--- tests.py
+++ tests.py
@@ -3,19 +3,25 @@ from contrived_func import contrived_func
 
 class TestContrivedFunc(unittest.TestCase):
 
-    def test_case_1(self):
+    def test_case_0(self):
         contrived_func(0)
 
-    def test_case_2(self):
+    def test_case_1(self):
         contrived_func(1)
 
-    def test_case_3(self):
+    def test_case_minus_1(self):
+        contrived_func(-1)
+
+    def test_case_100(self):
         contrived_func(100)
 
-    def test_case_4(self):
+    def test_case_large(self):
+        contrived_func(100000000)
+
+    def test_case_double(self):
         contrived_func(1.5)
 
-    def test_case_5(self):
+    def test_case_float(self):
         contrived_func(1.55)
 
 if __name__ == '__main__':
