2bbc1fccbcfd69440d11ad8b1ac5b70fdbab7161
Tea Kidder <teakidder@gmail.com>
2025-07-13 20:01:15 -0700

    wow that was fast

--- tests.py
+++ tests.py
@@ -4,10 +4,13 @@ from contrived_func import contrived_func
 class TestContrivedFunc(unittest.TestCase):
 
     def test_case_1(self):
-        contrived_func(0)  
+        contrived_func(0)
 
     def test_case_2(self):
         contrived_func(1)
+
+    def test_case_3(self):
+        contrived_func(-1)
 
 if __name__ == '__main__':
     unittest.main()
