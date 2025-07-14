2eda51806030abc6b6e3f6b50b8e82b4f10f627e
Tea Kidder <teakidder@gmail.com>
2025-07-13 20:10:13 -0700

    shrinkin it

--- tests.py
+++ tests.py
@@ -5,9 +5,6 @@ class TestContrivedFunc(unittest.TestCase):
 
     def test_case_0(self):
         contrived_func(0)
-    
-    def test_case_none(self):
-        contrived_func(None)
 
     def test_case_1(self):
         contrived_func(1)
@@ -18,14 +15,8 @@ class TestContrivedFunc(unittest.TestCase):
     def test_case_100(self):
         contrived_func(100)
 
-    def test_case_large(self):
-        contrived_func(100000000)
-
     def test_case_double(self):
         contrived_func(1.5)
-
-    def test_case_float(self):
-        contrived_func(1.55)
 
 if __name__ == '__main__':
     unittest.main()
