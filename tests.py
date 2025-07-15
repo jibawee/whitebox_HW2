ccf38cc0d8d58d964e4a866d7bc02b60d274d274
jibawee <teakidder@gmail.com>
2025-07-14 20:04:05 -0700

    trying to hit last couple tests

--- tests.py
+++ tests.py
@@ -21,6 +21,9 @@ class TestContrivedFunc(unittest.TestCase):
     def test_case_6(self):  # a=F, b=F, c=T, d=T
         contrived_func(-20)
 
+    def test_case_7(self):  # a=F, b=T, c=T, d=F
+        contrived_func(42)
+
     def test_case_8(self):  # a=F, b=F, c=F, d=F
         contrived_func(15)
 
